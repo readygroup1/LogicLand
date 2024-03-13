3a072e465bb850629d49bbed1393614c9414bd6d
Callum Thompson <cthom226@uwo.ca>
2024-03-13 10:55:19 -0400

    Classroom highscore table is now working and pushed. You can sign up as an admin and create a new class. People can join that class and their highscore is displayed. Next, I am going to make the global highscores, plus some login restrictions for the username and 3 letter initials etc...

--- LogicLand/src/application/AccountManager.java
+++ LogicLand/src/application/AccountManager.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class AccountManager {
@@ -53,9 +54,16 @@ public class AccountManager {
 		return db.getPlayer(playerID);
 	}
 	
+	public static int getCurrentClassID() {
+		if(AccountManager.isAdmin) {
+			return db.getClassIDAdmin(AccountManager.currentUserID);
+		} else {
+			return db.getClassID(AccountManager.currentUserID);
+		}
+	}
 	
 	public static boolean verifyLogin(String name, String password, boolean isAdmin) {
-		if(AccountManager.isAdmin) {
+		if(isAdmin) {
 			if(db.verifyAdmin(name, password)) {
 				AccountManager.currentUserID = db.getAdminID(name);
 				AccountManager.isAdmin = true;
@@ -69,6 +77,46 @@ public class AccountManager {
 			}
 		}
 		return false;
+	}
+	
+	public static ArrayList<String> getTopFiveNames() {
+		ArrayList<String> list = null;
+		try {
+			list = db.getTopFiveNames();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	public static ArrayList<String> getTopFiveNames(int classID) {
+		ArrayList<String> list = null;
+		try {
+			list = db.getTop5NamesClassroom(classID);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	public static ArrayList<Integer> getTopFiveScores() {
+		ArrayList<Integer> list = null;
+		try {
+			list = db.getTopFiveScores();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	public static ArrayList<Integer> getTopFiveScores(int classID) {
+		ArrayList<Integer> list = null;
+		try {
+			list = db.getTop5ScoresClassroom(classID);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
 	}
 	
 }
