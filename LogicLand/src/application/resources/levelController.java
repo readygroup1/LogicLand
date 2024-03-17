948c7e359cf3046c0b862a315379e726b3a954f9
Kalundi Serumaga <kserumag@uwo.ca>
2024-03-16 21:40:04 -0400

    Added JavaDoc to leveController, and if my previous push did not go through I also: Created a stage for each level, with working back, previouslevel, and nextlevel buttons They are dependent on the levelLabel, importatn to keep in mind. See levelcontroller

--- LogicLand/src/application/resources/levelController.java
+++ LogicLand/src/application/resources/levelController.java
@@ -13,15 +13,24 @@ public class levelController {
     SceneSwitcher sceneSwitcher = new SceneSwitcher();
 
     // Store the current level
-    private int currentLevel; // Default to Level 1
+    private int currentLevel; // currentLevel as its integer value
 
     @FXML
     private Label currentLevelLabel; // Inject the Label from FXML
 
-    // Button Functions
-    @FXML
-    private Button backButton;
+    
+    
+    
+   
+  
+    // Button and Helper Functions
 
+    /**
+     * Switches the scene to the roadmap view when the "Back" button is clicked.
+     *
+     * @param event The ActionEvent triggered by clicking the "Back" button.
+     * @throws IOException If an I/O exception occurs during scene switching.
+     */
     public void back(ActionEvent event) throws IOException {
         try {
             sceneSwitcher.switchScene(event, "roadmap.fxml");
@@ -30,25 +39,33 @@ public class levelController {
         }
     }
 
+    /**
+     * Switches the scene to the previous level view when the "Previous Level" button is clicked.
+     * 
+     *
+     * @param event The ActionEvent triggered by clicking the "Previous Level" button.
+     * @throws IOException If an I/O exception occurs during scene switching.
+     */
     public void previousLevel(ActionEvent event) throws IOException {
         try {
             currentLevel = extractLevelNumber(currentLevelLabel.getText());
-
-            if (currentLevel > 1) {
-                currentLevel--;
-                String previousLevelFXML = "Level" + currentLevel + ".fxml";
-                sceneSwitcher.switchScene(event, previousLevelFXML);
-            } else {
-                System.out.println("Already at the first level.");
-            }
+            currentLevel--;
+            String previousLevelFXML = "Level" + currentLevel + ".fxml";
+            sceneSwitcher.switchScene(event, previousLevelFXML);
         } catch (IOException exception) {
             exception.printStackTrace();
         }
     }
 
+    /**
+     * Switches the scene to the next level view when the "Next Level" button is clicked.
+     *
+     * @param event The ActionEvent triggered by clicking the "Next Level" button.
+     * @throws IOException If an I/O exception occurs during scene switching.
+     */
     public void nextLevel(ActionEvent event) throws IOException {
         try {
-        	currentLevel = extractLevelNumber(currentLevelLabel.getText());
+            currentLevel = extractLevelNumber(currentLevelLabel.getText());
             currentLevel++;
             String nextLevelFXML = "Level" + currentLevel + ".fxml";
             sceneSwitcher.switchScene(event, nextLevelFXML);
@@ -57,7 +74,13 @@ public class levelController {
         }
     }
 
-    // Method to extract the level number from the label text
+    /**
+     * Extracts the level number from the given label text.
+     * Assumes the label text is in the format "LevelX".
+     *
+     * @param labelText The text of the label, assumed to be in the format "LevelX".
+     * @return The extracted level number.
+     */
     private int extractLevelNumber(String labelText) {
         return Integer.parseInt(labelText.substring(5)); // Assuming "LevelX" format
     }
