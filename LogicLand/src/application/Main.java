6905044c3ac21a38e341bb92a861fb58e519cdc7
Nick Howard <nhowar@uwo.ca>
2024-03-19 12:02:18 -0400

    forgot to reset,  skip login to false, also some formatting in fxml

--- LogicLand/src/application/Main.java
+++ LogicLand/src/application/Main.java
@@ -13,7 +13,7 @@ import javafx.scene.text.Font;
 public class Main extends Application {
 	
 	// For DEvelopment. REmove before hanging in.
-	boolean skipLogin = true;
+	boolean skipLogin = false;
 	@Override
 	
 	// This is boiler plate code to open up a window. 
