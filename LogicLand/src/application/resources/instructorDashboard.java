4e1de43c36e3f054b8a0d10a46568df18b307655
Callum Thompson <cthom226@uwo.ca>
2024-03-20 12:42:09 -0400

    Quick fix, started the admin dashboard

--- LogicLand/src/application/resources/instructorDashboard.java
+++ LogicLand/src/application/resources/instructorDashboard.java
@@ -1,15 +1,27 @@
 package application.resources;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import application.AccountManager;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
 
-public class instructorDashboard {
+public class instructorDashboard implements Initializable{
 
 	audioPlayer audio = new audioPlayer();
 	SceneSwitcher sceneSwitcher = new SceneSwitcher();
 	
+	//FX variables
+	@FXML
+	Text className;
 	
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+		className.setText("Class: "+ AccountManager.getClassName(AccountManager.getCurrentClassID()));
+	}
 	
 	
 	//-------------------Buttons-------------------------------
